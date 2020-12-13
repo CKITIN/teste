25f8927f346c0bdd8583b2d3ddecd3b41615c9e9
CKITIN <cristiansouzau@gmail.com>
2020-12-12 22:24:10 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+	<form action="login.php" method="POST">
+
+		<span>Nome: </span><input type="text" name="usuario" id="nome" style="margin-bottom: 10px;"> <br>
+
+		<span>Senha: </span><input type="password" name="senha" id="senha" > <br>
+
+		<input type="submit" name="enviar" id="enviar" value="enviar" style="margin-top: 10px; margin-left: 10px;"> <br>
+	</form>
+</body>
+</html>
---
+++ painel.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Painel</title>
+</head>
+<body>
+	<p>Painel</p>
+
+</body>
+</html>
